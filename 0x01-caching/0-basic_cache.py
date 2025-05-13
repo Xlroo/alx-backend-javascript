bdca87e0a64bf0251952b2515dd2326feab579a4
Xlroo <>
2025-05-13 19:30:53 +0000

    0. Basic dictionary

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import BaseCaching as basecaching
+
+def BasicCache(self,print_cache, key, item):
+    """
+    """
+    def put(self,key,item):
+        if key:
+            self.cache_data = item
+        else :
+            return ""
+
+    def get(self, key):
+        if key:
+            return self.cache_data
+        else:
+            return None
+        
+
+
